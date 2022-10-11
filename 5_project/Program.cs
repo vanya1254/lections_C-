1d63311fe86bb92cf150a5422b1ea65d114fa59d
Ivan <vanya1254@yandex.ru>
2022-10-11 20:35:02 +0300

    add 5_project

---
+++ 5_project/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "света")
+{
+    Console.WriteLine("Hello, sister!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
