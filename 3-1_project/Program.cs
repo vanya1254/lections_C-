3320293fa95e1cac6553b8822273cc2077f9e266
Ivan <vanya1254@yandex.ru>
2022-10-11 20:23:07 +0300

    add 3-1_project

---
+++ 3-1_project/Program.cs
@@ -0,0 +1,6 @@
+﻿int num1 = new Random().Next(1, 10); // Случайное число 1 ... 9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int result = num1 + num2;
+Console.Write(result);
