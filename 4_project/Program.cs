cb22f2b36fedb6f35926f64f14883b9cffc71a97
Ivan <vanya1254@yandex.ru>
2022-10-11 20:09:51 +0300

    add 4_project

---
+++ 4_project/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введи первое число ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи второе число ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(num1 / num2);
