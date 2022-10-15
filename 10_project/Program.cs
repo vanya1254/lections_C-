4ddeea0371619ff81ccbfe84dc169179433a5f21
Ivan <vanya1254@yandex.ru>
2022-10-15 12:52:24 +0300

    add 10_project

---
+++ 10_project/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 54, 57, 94, 32, 65, 76, 94};
+
+int n = array.Length;
+int find = 94;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
